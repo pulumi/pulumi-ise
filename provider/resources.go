f96a8381f4e8ff14d68c9580eb57517bf25664a3
Ian Wahbe <ian@wahbe.com>
2024-05-09 12:38:43 -0700

    Simplify and correct naming

--- provider/resources.go
+++ provider/resources.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2024, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,20 +16,17 @@ package provider
 
 import (
 	"fmt"
-	"path/filepath"
+	"path"
 	"strings"
-	"unicode"
 
 	// embed is used to store bridge-metadata.json in the compiled binary
 	_ "embed"
 
 	"github.com/CiscoDevNet/terraform-provider-ise/ise"
-	"github.com/ettle/strcase"
 
 	pf "github.com/pulumi/pulumi-terraform-bridge/pf/tfbridge"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
-	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
-	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/tokens"
 
 	"github.com/pulumi/pulumi-ise/provider/pkg/version"
 )
@@ -53,6 +50,7 @@ var moduleNames = map[string]string{
 	"endpoint":         modIdentityManagement,
 	"identity":         modIdentityManagement,
 	"network_access":   modNetworkAccess,
+	"network":          modNetwork,
 	"tacacs":           modDeviceAdmin,
 	"trustsec":         modTrustSec,
 }
@@ -64,110 +62,27 @@ var resourceModules = map[string]string{
 	"downloadable_acl":         modNetworkAccess,
 	"internal_user":            modIdentityManagement,
 	"license_tier_state":       modSystem,
-	"network_device":           modNetwork,
-	"network_device_group":     modNetwork,
 	"repository":               modSystem,
 	"user_identity_group":      modIdentityManagement,
 }
 
-var namespaceMap = map[string]string{}
-
-func convertName(tfname string) (module string, name string) {
-	tfNameItems := strings.Split(tfname, "_")
-	contract.Assertf(len(tfNameItems) >= 2, "Invalid snake case name %s", tfname)
-	contract.Assertf(tfNameItems[0] == "ise", "Invalid snake case name %s. Does not start with ise", tfname)
-	fullName := strings.Join(tfNameItems[1:], "_")
-	moduleTitle := ""
-	if mod, ok := resourceModules[fullName]; ok {
-		moduleTitle = mod
-	} else {
-		for mod, val := range moduleNames {
-			if strings.HasPrefix(fullName, mod) {
-				moduleTitle = val
-				break
-			}
-		}
-		if moduleTitle == "" {
-			contract.Failf("could not determine module for %q", tfname)
-		}
-	}
-	name = strcase.ToPascal(strings.Join(tfNameItems[1:], "_"))
-	name = trimPrefixCaseInsensitive(name, moduleTitle)
-	module = strings.ToLower(moduleTitle)
-	namespaceMap[module] = moduleTitle
-
-	contract.Assertf(!unicode.IsDigit(rune(module[0])), "Pulumi namespace must not start with a digit: %s", name)
-	contract.Assertf(!unicode.IsDigit(rune(name[0])), "Pulumi name must not start with a digit: %s", name)
-	return
-}
-
-func trimPrefixCaseInsensitive(s, prefix string) string {
-	if strings.HasPrefix(strings.ToLower(s), strings.ToLower(prefix)) {
-		return s[len(prefix):]
-	}
-	return s
-}
-
-func makeDataSource(ds string) tokens.ModuleMember {
-	mod, name := convertName(ds)
-	return tfbridge.MakeDataSource("ise", mod, "get"+name)
-}
-
-func makeResource(res string) tokens.Type {
-	mod, name := convertName(res)
-	return tfbridge.MakeResource("ise", mod, name)
-}
-
-func moduleComputeStrategy() tfbridge.Strategy {
-	return tfbridge.Strategy{
-		Resource: func(tfToken string, elem *tfbridge.ResourceInfo) error {
-			elem.Tok = makeResource(tfToken)
-			return nil
-		},
-		DataSource: func(tfToken string, elem *tfbridge.DataSourceInfo) error {
-			elem.Tok = makeDataSource(tfToken)
-			return nil
-		},
-	}
-}
-
 // Provider returns additional overlaid schema and metadata associated with the provider..
 func Provider() tfbridge.ProviderInfo {
-	// Instantiate the Terraform provider
-	p := pf.ShimProvider(ise.Provider())
-
-	// Create a Pulumi provider mapping
 	prov := tfbridge.ProviderInfo{
-		P:    p,
-		Name: "ise",
-		// DisplayName is a way to be able to change the casing of the provider
-		// name when being displayed on the Pulumi registry
+		P:           pf.ShimProvider(ise.Provider()),
+		Name:        "ise",
 		DisplayName: "Cisco ISE",
-		// The default publisher for all packages is Pulumi.
-		// Change this to your personal name (or a company name) that you
-		// would like to be shown in the Pulumi Registry if this package is published
-		// there.
-		Publisher: "pulumi",
-		// LogoURL is optional but useful to help identify your package in the Pulumi Registry
-		// if this package is published there.
-		//
-		// You may host a logo on a domain you control or add an SVG logo for your package
-		// in your repository and use the raw content URL for that file as your logo URL.
+		Publisher:   "pulumi",
 		LogoURL:     "https://raw.githubusercontent.com/pulumi/pulumi-ise/main/docs/ise.png",
 		Description: "A Pulumi package for managing resources on a Cisco ISE (Identity Service Engine) instance.",
-		// category/cloud tag helps with categorizing the package in the Pulumi Registry.
-		// For all available categories, see `Keywords` in
-		// https://www.pulumi.com/docs/guides/pulumi-packages/schema/#package.
 		Keywords: []string{
 			"pulumi",
 			"ise",
 			"category/network",
 		},
-		License:    "Apache-2.0",
-		Homepage:   "https://github.com/pulumi/pulumi-ise",
-		Repository: "https://github.com/pulumi/pulumi-ise",
-		// The GitHub Org for the provider - defaults to `terraform-providers`. Note that this
-		// should match the TF provider module's require directive, not any replace directives.
+		License:           "Apache-2.0",
+		Homepage:          "https://github.com/pulumi/pulumi-ise",
+		Repository:        "https://github.com/pulumi/pulumi-ise",
 		GitHubOrg:         "CiscoDevNet",
 		UpstreamRepoPath:  "./upstream",
 		MetadataInfo:      tfbridge.NewProviderMetadata(bridgeMetadata),
@@ -176,7 +91,6 @@ func Provider() tfbridge.ProviderInfo {
 		Config:            map[string]*tfbridge.SchemaInfo{},
 		Resources: map[string]*tfbridge.ResourceInfo{
 			"ise_active_directory_add_groups": {
-				Tok: makeResource("ise_active_directory_add_groups"),
 				Fields: map[string]*tfbridge.SchemaInfo{
 					"groups": {
 						CSharpName: "GroupsValue",
@@ -206,7 +120,7 @@ func Provider() tfbridge.ProviderInfo {
 			return i
 		})(),
 		Golang: &tfbridge.GolangInfo{
-			ImportBasePath: filepath.Join(
+			ImportBasePath: path.Join(
 				fmt.Sprintf("github.com/pulumi/pulumi-%[1]s/sdk/", "ise"),
 				tfbridge.GetModuleMajorVersion(version.Version),
 				"go",
@@ -218,15 +132,65 @@ func Provider() tfbridge.ProviderInfo {
 			PackageReferences: map[string]string{
 				"Pulumi": "3.*",
 			},
-			Namespaces: namespaceMap,
 		},
 		Java: &tfbridge.JavaInfo{
 			BasePackage: "com.pulumi",
 		},
 	}
 
-	prov.MustComputeTokens(moduleComputeStrategy())
+	cSharpNamespaces := map[string]string{}
+	prov.MustComputeTokens(tokens.MappedModules("ise_",
+		"index", moduleNames, noGet(func(module, name string) (string, error) {
+			switch name {
+			case "":
+				name = module
+			case "AllowedProtocols":
+				module = modNetworkAccess
+			case "AllowedProtocolsTacacs":
+				module = modDeviceAdmin
+			case "AuthorizationProfile":
+				module = modNetworkAccess
+			case "DownloadableAcl":
+				module = modNetworkAccess
+			case "InternalUser":
+				module = modIdentityManagement
+			case "LicenseTierState":
+				module = modSystem
+			case "NetworkDevice":
+				module = modNetwork
+			case "NetworkDeviceGroup":
+				module = modNetwork
+			case "Repository":
+				module = modSystem
+			case "UserIdentityGroup":
+				module = modIdentityManagement
+			}
+			if module == "index" {
+				return "", fmt.Errorf("Missing module mapping for %s", name)
+			}
+
+			cSharpNamespaces[strings.ToLower(module)] = module
+
+			return string(tfbridge.MakeResource(prov.Name, strings.ToLower(module), name)), nil
+		})))
+	prov.CSharp.Namespaces = cSharpNamespaces
 	prov.SetAutonaming(255, "-")
 
 	return prov
+}
+
+func noGet(f tokens.Make) tokens.Make {
+	return func(module, name string) (string, error) {
+		if rest, ok := strings.CutPrefix(name, "get"); ok {
+			tk, err := f(module, rest)
+			if err != nil {
+				return "", err
+			}
+			parts := strings.Split(tk, ":")
+			parts[2] = "get" + parts[2]
+
+			return strings.Join(parts, ":"), nil
+		}
+		return f(module, name)
+	}
 }
